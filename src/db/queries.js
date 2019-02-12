a00a1f88ba953e6f21abcd2d5c0b7591acae6a45
Gabriela Budeanu <gabriela.budeanu@gmail.com>
2019-02-12 21:16:02 +0000

    Update queries to database

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,13 +1,12 @@
 const knex = require('./knex');
-const { errorHandler } = require('../helper');
 
 async function createConference({ name }) {
   try {
     const response = await knex.insert({ name }).into('conferences').returning('*');
     return response;
   } catch (error) {
-    const errorHandled = errorHandler(error);
-    throw errorHandled;
+    console.log('Error in knex queries #createConference', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
   }
 }
 
@@ -25,8 +24,89 @@ async function createTalk({
     }).into('talks').returning('*');
     return response;
   } catch (error) {
-    const errorHandled = errorHandler(error);
-    throw errorHandled;
+    console.log('Error in knex queries #createTalk', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
+  }
+}
+
+async function createSpeaker({
+  talkID, firstName, lastName, email, bio, photoUrl
+}) {
+  try {
+    const response = await knex.insert({
+      talk_id: talkID,
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      bio,
+      photo_url: photoUrl
+    }).into('speakers').returning('*');
+    return response;
+  } catch (error) {
+    console.log('Error in knex queries #createSpeaker', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
+  }
+}
+
+async function createAttendee({
+  conferenceID, firstName, lastName, email
+}) {
+  try {
+    const response = await knex.insert({
+      conference_id: conferenceID,
+      first_name: firstName,
+      last_name: lastName,
+      email
+    }).into('attendees').returning('*');
+    return response;
+  } catch (error) {
+    console.log('errror', error);
+    console.log('Error in knex queries #createAttendee', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
+  }
+}
+
+async function getMaximumAttendancePerTalk({
+  talkID
+}) {
+  try {
+    const response = await knex.select('maximum_attendance').from('talks').where({ id: talkID });
+    return response[0].maximum_attendance;
+  } catch (error) {
+    console.log('Error in knex queries #getMaximumAttendancePerTalk', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
+  }
+}
+
+async function listByTalkID({
+  tableName, talkID
+}) {
+  try {
+    const response = await knex.select().from(`${tableName}`).where({ talk_id: talkID });
+    return response;
+  } catch (error) {
+    console.log('Error in knex queries #listByTalkID', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
+  }
+}
+
+async function createRsvp({
+  talkID, attendeeID
+}) {
+  try {
+    const maxAttendancePerTalk = await getMaximumAttendancePerTalk({ talkID });
+    const rsvpsPerTalk = await listByTalkID({ tableName: 'rsvps', talkID });
+    if (rsvpsPerTalk.length < maxAttendancePerTalk) {
+      const response = await knex.insert({
+        talk_id: talkID,
+        attendee_id: attendeeID
+      }).into('rsvps').returning('*');
+      return response;
+    }
+    throw new Error('Talk fully booked');
+  } catch (error) {
+    console.log('Error in knex queries #createRsvp', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
   }
 }
 
@@ -35,12 +115,17 @@ async function list(tableName) {
     const response = await knex.select().from(`${tableName}`);
     return response;
   } catch (error) {
-    const errorHandled = errorHandler(error);
-    throw errorHandled;
+    console.log('Error in knex queries #list', `with following error message: ${error.message}`);
+    throw new Error(`${error.message}`);
   }
 }
+
 module.exports = {
   createConference,
   createTalk,
-  list
+  createSpeaker,
+  createAttendee,
+  createRsvp,
+  list,
+  listByTalkID
 };
