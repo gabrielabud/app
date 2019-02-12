7327c0ff01c49b31953afd700a6fbd4e9ed8623a
Gabriela Budeanu <gabriela.budeanu@gmail.com>
2019-02-12 21:20:04 +0000

    Update routes aggregator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const conferences = require('./routes/conferences');
 const talks = require('./routes/talks');
+const speakers = require('./routes/speakers');
+const attendees = require('./routes/attendees');
+const rvsps = require('./routes/rsvps');
 
 const app = express();
 const port = 3000;
@@ -12,6 +15,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Welcome to our FinTech Conference'));
 app.use('/', conferences);
 app.use('/', talks);
+app.use('/', speakers);
+app.use('/', attendees);
+app.use('/', rvsps);
 
 module.exports = {
   run: () => app.listen(port, () => console.log(`FinTech Conference listening on port ${port}!`)),
